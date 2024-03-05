7b8919dacc16116cc110a6b587a258cff53f1bf6
ChunYen-Chen <chunyenchen2019@gmail.com>
2024-03-05 15:52:06 +0800

    Bug fix of the animation

--- js/openingAnimation.js
+++ js/openingAnimation.js
@@ -14,18 +14,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 // select video element
-var vid = document.getElementById('halo-mp4');
-var vid_div = document.getElementById('halo-div');
-var nav = document.getElementById('nav-bar');
-//var vid = $('#v0')[0]; // jquery option
-
-// pause video on load
-vid.pause();
-
-// pause video on document scroll (stops autoplay once scroll started)
-window.onscroll = function(){
-    vid.pause();
-};
+//var vid = document.getElementById('halo-mp4');
+//var vid_div = document.getElementById('halo-div');
+//var nav = document.getElementById('nav-bar');
+////var vid = $('#v0')[0]; // jquery option
+//
+//// pause video on load
+//vid.pause();
+//
+//// pause video on document scroll (stops autoplay once scroll started)
+//window.onscroll = function(){
+//    vid.pause();
+//};
 
 // refresh video frames on interval for smoother playback
 //setInterval(function(){
